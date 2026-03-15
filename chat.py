51979447f5b8e97ff84d6f703d4001c6da5abd1a
Madhu <krishchirghute@gmail.com>
2026-03-15 21:49:41 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,35 @@
+import socket
+
+host = "127.0.0.1"
+port = 5000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen(1)
+
+print("Server started. Waiting for connection...")
+
+conn, addr = server.accept()
+print("Connected with", addr)
+
+while True:
+    message = conn.recv(1024).decode()
+    print("Client:", message)
+
+    reply = input("You: ")
+    conn.send(reply.encode())
+
+    import socket
+
+host = "127.0.0.1"
+port = 5000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+while True:
+    message = input("You: ")
+    client.send(message.encode())
+
+    reply = client.recv(1024).decode()
+    print("Server:", reply)
